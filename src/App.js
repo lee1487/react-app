3b940f1b35baa6fd68d23f732a94a824b40e43f5
이현세 <dlgustp1487@naver.com>
2021-01-08 17:27:17 +0900

    component,props,state 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+import Subject from "./components/Subject"
 import './App.css';
-import { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'Web', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
   }  
