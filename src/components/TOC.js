57527d50625aa540706f27e6ef3ed3eb3353f61d
이현세 <dlgustp1487@naver.com>
2021-01-15 13:02:45 +0900

    component이벤트 만들기 까지

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
    render() {
+    console.log('Toc render');
     let lists = []
     let data = this.props.data
     let i = 0;
     while (i < data.length) {
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+        lists.push(
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              onClick={function(id, e) {
+                e.preventDefault();
+                this.props.onChangePage(id);
+              }.bind(this,data[i].id)}
+            >{data[i].title}</a>
+          </li>)
         i = i + 1;
     }
      return (
